105c9ed260dcf4a7e6ea60de15b59f3c8f26a132
nazimfeni <mdnazimuddin77@gmail.com>
2023-11-09 21:23:49 +0600

    update UI screen

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,32 @@ class TaskManagerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: SplashScreen(),
+      theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+        ),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        primaryColor: Colors.green,
+        primarySwatch: Colors.green,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+
+          )
+        )
+      ),
     );
   }
 }
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager/ui/screens/login_screen.dart';
+import 'package:task_manager/ui/widgets/body_background.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,21 +11,35 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    goToLogin();
+  }
+
+  void goToLogin() {
+    Future.delayed(const Duration(seconds: 2)).then((value) {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+          (route) => false);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SvgPicture.asset(
-            'assets/images/background.svg',
-            width: double.infinity,
-            height: double.infinity,
+      body: BodyBackground(
+        child: Center(
+          child: SvgPicture.asset(
+            'assets/images/app-logo.svg',
+            width: 120,
           ),
-          Center(
-            child: SvgPicture.asset('assets/images/app-logo.svg'),
-          )
-        ],
-      ),
-    );
+        ),
+      )
+      );
+
   }
 }
